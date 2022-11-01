5d1d1403eb35ee886eeec66806bae8dfa3569ddd
Hariom Singh <94778964+Hariom-Anilkumar-Singh@users.noreply.github.com>
2022-11-01 12:07:27 +0530

    Add files via upload

--- src/api/getresponse.php
+++ src/api/getresponse.php
@@ -1,3 +1,50 @@
 <?php
+require_once "../config.php";
+error_reporting(0);
+$json=file_get_contents('php://input');
+$Id = json_decode($json,true)['Id'];
+$decode_array = json_decode($json,true)['responses'];
+  
+$statusResponse=Array();
+$statusResp="success";
 
+if(!empty($decode_array) && isset($Id)){
+
+    // echo $Id;
+    // print_r($decode_array);
+// if(isset($_POST['Id']) && isset($_POST['QuestionId']) && isset($_POST['Response']) ){
+    foreach($decode_array as $response_array){
+        $QuestionId = $response_array['QuestionId'];
+        $Response = $response_array['Response'];    
+        
+        $sql = "INSERT INTO responses(Id, QuestionId, Response) VALUES (?,?,?)";
+
+        if($stmt = $mysqli->prepare($sql)){
+            
+            // $Id = $_POST['Id'];
+            // $QuestionId = $_POST['QuestionId'];
+            // $Response = $_POST['Response'];
+            $stmt->bind_param("iis", $Id, $QuestionId, $Response);
+            if($stmt -> execute()){
+                //if query works successfully return no  
+            }else{
+
+                $statusResp="failed";
+                response("Failure");
+                break;
+            }
+        }
+    }
+    response($statusResp);
+}else{
+    response("Missing parameters.");
+}
+
+function response($signoffMessage)
+{
+
+	$response['res'] = $signoffMessage;
+	$json_response = json_encode($response);
+	echo $json_response;
+}
 ?>
