8a53de2d5b23c03ba1c6c402863c5f07110a0505
Shubham Jadhav <shub13711jadhav@gmail.com>
2022-11-03 15:14:24 +0530

    Have added dropdown logic

--- src/api/getquestion.php
+++ src/api/getquestion.php
@@ -1,24 +1,63 @@
 <?php
 require_once "../config.php";
 
+function returnDropDownOptions($mysqli, $QuestionId){
+    $optionId = "";
+    $optionText = "";
+    $sql = 'SELECT OptionId, OptionText FROM DropDownOptions WHERE Status = ? AND QuestionId = ?';
+    $options = array();
+    if($stmt = $mysqli->prepare($sql)){
+        $status = 1;
+        $temp = array();
+        $stmt->bind_param("is",  $status, $QuestionId);
+        if($stmt -> execute()){
+            $stmt->store_result();
+            $stmt -> bind_result($optionId, $optionText);
+            if($stmt -> num_rows()>0){
+                while($stmt -> fetch()) {
+                    $temp = array("OptionId"=>$optionId, "OptionText"=>$optionText);
+                    array_push($options, $temp);
+                    
+                }
+                return $options;
+            }else{
+                return $options;
+            }
+        }else{
+            return $options = "Fail".mysqli_error($mysqli);
+        }
+    }else{
+        return $options = "Fail".mysqli_error($mysqli);
+    }
+
+}
+
 if(isset($_POST['Requirement'])){
-    $sql = 'SELECT QuestionId, Question FROM masterquestions WHERE (Requirement = ? OR Requirement = ? ) AND Status = ?';
+    $sql = 'SELECT QuestionId, Question, ResponseType FROM masterquestions WHERE (Requirement = ? OR Requirement = ? ) AND Status = ?';
 
     if($stmt = $mysqli->prepare($sql)){
         
         $requirement = $_POST['Requirement'];
-        $generalRequirement = 'general';
+        $generalRequirement = 'General';
         $status=1;
         $stmt->bind_param("ssi", $requirement, $generalRequirement, $status);
         if($stmt -> execute()){
             //store results
             $stmt->store_result();
-            $stmt -> bind_result($questionId, $question);
+            $stmt -> bind_result($questionId, $question, $responseType);
             $questionarr = array();
             if($stmt -> num_rows()>0)
             {
                 while($stmt -> fetch()) {
-                    $temp = array("QuestionId"=>$questionId, "Question"=>$question);
+                    if($responseType){
+                        $options = array(returnDropDownOptions($mysqli, $questionId));
+                        $temp = array("QuestionId"=>$questionId, "Question"=>$question, "ResponseType"=>$responseType, "Options"=>$options);
+                    }else{
+                        $temp = array("QuestionId"=>$questionId, "Question"=>$question, "ResponseType"=>$responseType);
+                    }
+
+                    
+
                     array_push($questionarr, $temp);
                 }
                 response($questionarr);
