c9b72bb9ef918c9cb202b896ba5c2b074b09288a
Hariom Singh <94778964+Hariom-Anilkumar-Singh@users.noreply.github.com>
2022-11-17 18:05:48 +0530

    added thankyou.php page

---
+++ src/thankyou.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Thank You</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://kit.fontawesome.com/577845f6a5.js" crossorigin="anonymous">
+    </script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="../style/thank_you.css">
+    <style>
+        /* * {
+            border: 1px solid red;
+        } */
+
+        body {
+            background-color: black;
+            font: 14px sans-serif;
+            text-align: center;
+        }
+    </style>
+
+</head>
+
+<body>
+    
+    <!-- conatainer 1 -- start  -->
+    <div class="conatainer1">
+        <!-- <img class="logo-img" src="../assets/logo.png" width="130" height="130" ALT="align box" ALIGN=CENTER> -->
+        <img class="icon-img" src="../assets/Vector.png" alt="right_icon">
+        <!-- <img class="righticon2" src="../assets/Polygon.png" alt="right_icon"> -->
+    </div>
+    <!-- conatainer 1 -- end  -->
+
+    <!-- conatainer 2 -- start  -->
+    <div class="conatainer2">
+        <p class="left-text"> ExperientialEtc </p>
+        <p class="right-text"> We are Creative </p>
+    </div>
+    <!-- conatainer 2 -- end  -->
+
+    <!-- conatainer 3 -- start  -->
+    <div class="conatainer3">
+        <img class="top-line" src="../assets/Line.png" alt="top_line">
+        <p class="thankyou-text"> THANK YOU. </p>
+        <p class="bold-text">we’ve just sent you an email containing NDA Document.</p>
+        <p class="normal-text">No Email ? Be sure to check your spam folder . you can contact us at : marketing@experientialetc.com</p>
+        <p class="bottom-text">Follow Us & Stay Informed </p>
+        <img class="bottom-line" src="../assets/Line.png">
+    </div>
+    <!-- conatainer 3 -- end  -->
+    
+</body>
+
+</html>
